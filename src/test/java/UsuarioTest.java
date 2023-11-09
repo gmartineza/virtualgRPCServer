3b8dc4ffd5275504b0d58867863d5fb79ba051b3
felix toledo <felixtoledoctes@gmail.com>
2023-11-09 13:00:01 -0300

    test for user created

--- src/test/java/UsuarioTest.java
+++ src/test/java/UsuarioTest.java
@@ -1,2 +1,20 @@
+import logic.BuscadorDeUsuarios;
+import logic.ListaUsuarios;
+import logic.Usuario;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class UsuarioTest {
+    @Test
+    public void testUsuario(){
+        ListaUsuarios lista = new ListaUsuarios();
+        lista.anadirUsuario("Pedro", "pedro", "pedro");
+        BuscadorDeUsuarios buscador = new BuscadorDeUsuarios(lista);
+
+        assertEquals("pedro", lista.getLista().get(0).getContra());
+        assertEquals("pedro", lista.getLista().get(0).getUsuario());
+        assertEquals("Pedro", lista.getLista().get(0).getNombre());
+
+        assertTrue(buscador.existe("pedro", "pedro"));
+    }
 }
